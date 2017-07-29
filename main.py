ad7e23c0953c539fb2b1f707ab1ae5ea972e3383
sam-k <samuel.keogh@uqconnect.edu.au>
2017-07-29 17:02:37 +1000

    -Query example in update_points -Start of flask in main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import requests, flask
+import requests, flask, pymongo
 
-#get GeoJSON data from brimbank
-DATASETS = [('Brimbank Parks','http://data.gov.au/geoserver/playground/wfs?request=GetFeature&typeName=ckan_e8d3580c_3981_47ab_a675_573805c3fa86&outputFormat=json'),
-            ('Ballarat Indoor','http://data.gov.au/geoserver/ballarat-indoor-recreation-facilities/wfs?request=GetFeature&typeName=9006b1fc_9a36_425d_ae2a_6307c37c99c9&outputFormat=json'),
-            ]
+connection = pymongo.MongoClient('mongo.getsporty.space')
+db = connection.getsporty
+spherical_distance = 750/1000 #distance in kilometres
 
-if __name__ == '__main__':
-    print get_datasets()
+for ground in db.sportsgrounds.find({}):
+    if "Canberra" in ground['name']:
+        print ground
+
+app = flask.Flask(__name__)
