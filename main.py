755fa1c1960fe5696392f14deda4e5ababbfb760
sam-k <samuel.keogh@uqconnect.edu.au>
2017-07-29 21:54:41 +1000

    -Last backend for Saturday

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import requests, flask, pymongo
 
+from flask_restful import Resource, Api
+
 connection = pymongo.MongoClient('mongo.getsporty.space')
 db = connection.getsporty
-spherical_distance = 750/1000 #distance in kilometres
-
-for ground in db.sportsgrounds.find({}):
-    if "Canberra" in ground['name']:
-        print ground
 
 app = flask.Flask(__name__)
+
+@app.route('/geoloc')
+def geoloc():
+    return
+
+@app.route('/register')
+def register(json_data):
+    user_data = json_data.encode('json')
+    db.users.update({'user':user_data['user'],user_data,{'upsert':True})
+    return
+
+@app.route('/event')
+def new_event(json_data):
+    db.users.update()
+    return
+
+def push_notification():
+    flask.request()
+
+if __name__ == '__main__':
+    app.run()
